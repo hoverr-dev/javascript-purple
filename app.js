75ad875535c19e8b6294cadc387aed893dc5ab64
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-02-26 13:55:56 +0300

    Логические операторы

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-/* 
-  Методом prompt получите ответ пользователя
-  на вопрос "Сколько будет 7 + или -15?". Если ответ верен
-  выведите в консоли "Успех", если нет - "Вы - робот!",
-  а если он выведет "Я не робот", то тоже "Успех".
-*/
+const isAdmin = true;
+const canWrite = true;
 
-const res = prompt('Сколько будет 7 + или -15?');
+console.log(`Системный файл ${isAdmin && canWrite}`);
+console.log(`Обычный файл ${isAdmin || canWrite}`);
+console.log(`Инвертируем права админа ${!isAdmin}`);
 
-switch (true) {
-	case res === 'Я не робот':
-	case Number(res) === 22:
-	case Number(res) === -8:
-		console.log('Успех');
-		break;
-	default:
-		console.log('Вы робот!');
-}
+const isEdited = true;
+const isSuperAdmin = true;
+
+console.log(
+	`Системный файл с редактированием ${
+		isAdmin && canWrite && (!isEdited || isSuperAdmin)
+	}`
+);
