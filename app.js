4dcc23851cebf6a937927f992542f4141c5d0c61
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-02-27 09:19:07 +0300

    Функции в функциях

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-function canAccessWebsite(age) {
-	if (age < 18) {
-		return 'Нет';
-	}
-	return 'Да';
-}
-console.log(canAccessWebsite(19));
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
 
-const canAccessWebsite2 = age => (age < 18 ? 'Нет' : 'Да');
-console.log(canAccessWebsite2(19));
+function calculateW(present) {
+	return present * KG_IN_USD;
+}
+
+function calculateKm(distance) {
+	return distance * KM_IN_USD;
+}
+
+function getExchangePrice(present1, present2, distance) {
+	const price1 = calculateW(present1);
+	const price2 = calculateW(present2);
+	const distancePrice = calculateKm(distance);
+	return price1 + price2 + distancePrice;
+}
+console.log(getExchangePrice(1, 2, 10));
