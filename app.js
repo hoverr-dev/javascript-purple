654198c19d3b6d66e31031be3086fef3de15c263
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-02-28 10:34:03 +0300

    Цикл в цикле

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-/* 
-  Задача вывести в консоль строку 'Я люблю JS !' из массива,
-  проходя циклом в обратном порядке, не используя метод reverse.
-  const arr = ['!', 'JS', 'Люблю', 'Я'];
-*/
+// for (let i = 1; i < 5; i++) {
+// 	console.log(`Цикл 1 - ${i}`);
+// 	for (let j = 1; j < 5; j++) {
+// 		console.log(`Цикл 2 - ${j}`);
+// 	}
+// }
 
-const arr = ['!', 'JS', 'Люблю', 'Я'];
-const newArr = [];
+const tasks = [
+	[1, 'Задача 1'],
+	[2, 'Задача 2'],
+];
 
-for (let i = arr.length - 1; i >= 0; i--) {
-	newArr.push(arr[i]);
+for (let i = 0; i < tasks.length; i++) {
+	for (let j = 0; j < tasks[i].length; j++) {
+		console.log(tasks[i][j]);
+	}
 }
-
-console.log(newArr.join(' '));
