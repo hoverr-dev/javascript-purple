ef25b0ec7df9ba0ab75fa1b7a1dd1e55eece0b78
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-02-28 10:02:20 +0300

    Цикл for

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-/* 
-  Дан произвольны url - 
-  'https://purpleschool.ru/course/javascript'
-  Нужно сделать функцию, которая выводит в консоль:
-  - Протокол (https)
-  - Доменное имя (purpleschool.ru)
-  - Путь внутри сайта (/course/javascript)
-*/
-
-const url = 'https://purpleschool.ru/course/javascript';
-const url2 = 'https://purpleschool.ru/course/javascript';
-
-function getUrlData(link) {
-	const [protocol, _, host, ...path] = link.split('/');
-	if (protocol === 'https:' || protocol === 'http:') {
-		if (!host.includes('.')) return;
-		console.log(protocol, host, path);
-		console.log(`Протокол: ${protocol.split(':')[0]}`);
-		console.log(`Доменное имя: ${host}`);
-		console.log(`Путь внутри сайта: /${path.join('/')}`);
-	}
+for (let i = 1; i < 10; i++) {
+	console.log(`Наш баланс ${i}$`);
 }
-
-getUrlData(url);
