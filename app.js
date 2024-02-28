4cdec030b241814601432630a7868c4fdcc5c9ec
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-02-28 10:25:17 +0300

    Упражнение - Цикл в обратном порядке

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
+/* 
+  Задача вывести в консоль строку 'Я люблю JS !' из массива,
+  проходя циклом в обратном порядке, не используя метод reverse.
+  const arr = ['!', 'JS', 'Люблю', 'Я'];
+*/
 
-for (let i = 0; i < tasks.length; i++) {
-	if (tasks[i] === 'Задача 2') {
-		continue;
-	}
-	console.log(tasks[i]);
+const arr = ['!', 'JS', 'Люблю', 'Я'];
+const newArr = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+	newArr.push(arr[i]);
 }
 
-console.log('-------');
-
-for (let i = 0; i < tasks.length; i++) {
-	if (tasks[i] === 'Задача 2') {
-		break;
-	}
-	console.log(tasks[i]);
-}
+console.log(newArr.join(' '));
