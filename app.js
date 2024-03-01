ed3ef0ee898fbf265d512a9cd9932d9023ad6ffe
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-01 09:29:51 +0300

    Callback

--- app.js
+++ app.js
@@ -1,60 +1,27 @@
-/* 
-  Есть выгрузка операций пользователя
-  const operations = [1000, -700, 300, -500, 10000];
-  а так же начальный баланс в 100$
-  Необходимо сделать функции расчёта:
-  - Итогового баланса
-  - Наличие отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
-  - Расчёта среднего расхода и среднего дохода
-*/
-
-const operations = [1000, -700, 300, -500, 10000];
-const starterBalance = 100;
-
-function getBalance(arr, initialBalance) {
-	let balance = initialBalance;
-
-	for (const el of arr) {
-		balance += el;
-	}
-	return balance;
-}
-console.log(getBalance(operations, starterBalance));
-
-function negativeBalance(arr, initialBalance) {
-	let balance = initialBalance;
-	let isOk = true;
-
-	for (const el of arr) {
-		balance += el;
-
-		if (balance < 0) {
-			isOk = false;
-			break;
-		}
-	}
-	return isOk;
-}
-console.log(negativeBalance(operations, starterBalance));
-
-function averageBalance(arr) {
-	let positiveSum = 0;
-	let positiveCount = 0;
-	let negativeSum = 0;
-	let negativeCount = 0;
-
-	for (const el of arr) {
-		if (el > 0) {
-			positiveCount++;
-			positiveSum += el;
-		}
-
-		if (el < 0) {
-			negativeCount++;
-			negativeSum += el;
-		}
-	}
-	return [positiveSum / positiveCount, negativeSum / negativeCount];
+function add(a, b) {
+	return a + b;
 }
 
-console.log(averageBalance(operations));
+function subtract(a, b) {
+	return a - b;
+}
+
+function power(a, b) {
+	return a ** b;
+}
+
+// Функция высшего порядка
+function calculate(a, b, fn) {
+	console.log(fn.name);
+	const res = fn(a, b);
+	return res;
+}
+
+let res = calculate(3, 5, add);
+console.log(res);
+
+res = calculate(3, 5, subtract);
+console.log(res);
+
+res = calculate(3, 5, power);
+console.log(res);
