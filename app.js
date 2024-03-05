c1bc3d810ec8d61e5f81ef113865875087bedeb9
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-05 11:05:40 +0300

    Упражнение - реализация some

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+/* 
+  Написать функцию, которая возвращает true,
+  если элемент есть, и false, если нет.
+*/
+
 const arr = [2, 4, 4, 10, 20];
 
-let elGT5;
-// for (const el of arr) {
-// 	if (el > 5) {
-// 		elGT5 = el;
-// 		break;
-// 	}
-// }
-// console.log(elGT5);
+function some(array, element) {
+	const res = array.find(el => el === element);
+	return res === undefined ? false : true;
+}
+console.log(some(arr, 1));
 
-elGT5 = arr.find(el => el > 5);
-console.log(elGT5);
-
-elGT5Index = arr.findIndex(el => el > 5);
-console.log(elGT5Index);
+const result = arr.some(el => el === 2);
+console.log(result);
