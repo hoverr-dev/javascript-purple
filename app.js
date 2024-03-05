f89131fcbb86a5be55469fd876a52e9b432c210a
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-05 10:57:22 +0300

    find и findIndex

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-/* 
-  Найти среднее значение последоватлеьность
-  числа с помощью reduce
-*/
+const arr = [2, 4, 4, 10, 20];
 
-const arr = [2, 4, 4, 10];
+let elGT5;
+// for (const el of arr) {
+// 	if (el > 5) {
+// 		elGT5 = el;
+// 		break;
+// 	}
+// }
+// console.log(elGT5);
 
-const averageValue = arr.reduce((acc, el, i) => {
-	if (i != arr.length - 1) {
-		return acc + el;
-	} else {
-		return (acc + el) / arr.length;
-	}
-}, 0);
-console.log(averageValue);
+elGT5 = arr.find(el => el > 5);
+console.log(elGT5);
+
+elGT5Index = arr.findIndex(el => el > 5);
+console.log(elGT5Index);
