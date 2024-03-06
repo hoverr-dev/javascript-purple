f72f1bcda4298d109d5f8e93b7321fb2685d56b9
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-06 15:34:06 +0300

    Обращение к элементам

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-const userArray = ['Вася', 'Пупкин', 24];
 const user = {
 	name: 'Вася',
 	surname: 'Пупкин',
 	age: 24,
 	skills: ['Программирование', 'Готовка'],
-	1: 1,
+	eduBasic: 'Школа 10',
+	eduPro: 'МФТИ',
 };
+console.log(user.city);
+console.log(user.skills);
+console.log(user['skills']);
+const level = 'Pro';
+console.log(user['edu' + level]);
+
+// const res = prompt('Введите свойство');
+user.city = 'Москва';
+user['city'] = 'Москва';
+console.log(user.city);
+
+user.age = 30;
+user['age'] = 30;
+console.log(user);
