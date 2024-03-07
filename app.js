97d010019373e0277029ff12b8d25dba33c480ca
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-07 09:50:18 +0300

    Итерирование по объекту

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-const initialBalance = 7;
-
-const wallet = {
-	balance: initialBalance,
-	operations: [],
+const cities = {
+	msk: {
+		lt: 200,
+		temp: 25,
+	},
+	spb: {
+		lt: 100,
+		temp: 20,
+	},
 };
+
+let sumTemp = 0;
+let citiesCount = Object.keys(cities).length;
+
+// for (const key in cities) {
+// 	citiesCount++;
+// 	sumTemp += cities[key].temp;
+// }
+// console.log(sumTemp / citiesCount);
+
+for (const key of Object.keys(cities)) {
+	sumTemp += cities[key].temp;
+}
+console.log(sumTemp / citiesCount);
