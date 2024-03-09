5acbc792a5d04b4978142ff4aaabe80131b52437
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-09 09:42:15 +0300

    Пример примитивов и объектов

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const sum = 1;
-console.log('start');
-console.log(sum);
+'use strict';
 
-function add5(n) {
-	n = n + 5;
-	if (n > 100) {
-		return n;
-	}
-	console.log(n);
-	return add5(n);
-}
+const user = {
+	name: 'Anton',
+	id: 1,
+	roles: ['Admin'],
+};
 
-add5(sum);
-console.log('ended');
+// const newUser = Object.assign({}, user);
+// user.name = 'NewUser';
+// console.log(user);
+// console.log(newUser);
+
+const newUser2 = {
+	...user,
+};
+
+newUser2.name = 'NewUser';
+newUser2.roles.push('User');
+console.log(user);
+console.log(newUser2);
