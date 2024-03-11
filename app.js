35414d91a48301e04f14cf7feb26b0197e1f059b
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-11 12:20:17 +0300

    Упражнение - управление this

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 'use strict';
+/* 
+    Создайте объект пользователя с парлем.
+    С помощью функции ниже удалить пароль сделав
+    функцию сброса пароля
+*/
 
-const audi = {
-	make: 'Audi',
-	model: 'A3',
-	damages: [],
+function removePassword(reset) {
+	if (reset) {
+		this.password = undefined;
+	} else {
+		this.password = '1';
+	}
+}
+
+const user = {
+	login: 'user@.ru',
+	password: '12345',
 };
 
-const carManipulation = {
-	addDamage(part, rate) {
-		this.damages.push({ part, rate });
-		console.log(`Добавили повреждения на ${this.make} ${this.model}`);
-	},
-};
-
-const addDamageAudi = carManipulation.addDamage.bind(audi);
-addDamageAudi('Крыло', 3);
-
-const addDamageAudiRoof = carManipulation.addDamage.bind(audi, 'Крыша');
-addDamageAudiRoof(5);
-addDamageAudiRoof(3);
-console.log(audi);
+const resetUserPassword = removePassword.bind(user, true);
+resetUserPassword();
+console.log(user);
