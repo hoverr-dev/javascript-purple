cbaab8bd06ec2d5ad1d23fcf5051d413331d2761
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-11 08:59:19 +0300

    Arguments

--- app.js
+++ app.js
@@ -1,36 +1,19 @@
 'use strict';
+// let, var, const func, arguments
+// Scope chain
+// this
 
-const user = {
-	firstName: 'Вася',
-	lastName: 'Пупкин',
-	age: 20,
-	getUserInfo: function () {
-		console.log(this);
-		console.log(`${this.firstName} ${this.lastName}`);
+function sumNum(num1, num2) {
+	console.log(this);
+	console.log(arguments);
+	return num1 + num2;
+}
+console.log(sumNum(1, 4, 3, 7));
 
-		const canDrink = () => {
-			if (this.age >= 18) {
-				console.log('Может уже пить');
-			} else {
-				console.log('Не может пить');
-			}
-		};
-
-		// const self = this;
-		// function canDrink() {
-		// 	if (self.age >= 18) {
-		// 		console.log('Может уже пить');
-		// 	} else {
-		// 		console.log('Не может пить');
-		// 	}
-		// }
-		canDrink();
-	},
-	getUserInfoArrow: () => {
-		console.log(this);
-		console.log(`${this.firstName} ${this.lastName}`);
-	},
+const sumNumArr = (num1, num2) => {
+	console.log(this);
+	console.log(arguments);
+	return num1 + num2;
 };
 
-user.getUserInfo();
-// user.getUserInfoArrow();
+console.log(sumNumArr(1, 4, 3, 7));
