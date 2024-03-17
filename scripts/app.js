5a4f9715f1642edb0fc8f17305059c6af39ea6cb
Vladislav Nevzorov <hoverrdev@gmail.com>
2024-03-17 13:54:31 +0300

    Загрузка и сохранение данных

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,22 @@
 'use strict';
 
-function submitForm() {
-	const input = document.querySelector('.input').value;
-	if (!input) return;
-	document.querySelector('.panel').innerText = input;
-	document.querySelector('.input').value = '';
-	document
-		.querySelector('.notification')
-		.classList.remove('notification_hidden');
-	const textString = JSON.stringify({
-		text: input,
-	});
-	localStorage.setItem('text', textString);
-}
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
 
-function inputChanged(e) {
-	if (e.code === 'Enter') {
-		submitForm();
+/* utils */
+
+function loadData() {
+	const habbitsString = localStorage.getItem(HABBIT_KEY);
+	const habbitArray = JSON.parse(habbitsString);
+	if (Array.isArray(habbitArray)) {
+		habbits = habbitArray;
 	}
 }
+
+function saveData() {
+	localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+
+(() => {
+	loadData();
+})();
